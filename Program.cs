2614a88eef7fa395fb5152b772a38e3c04fa9e39
Omirzakmaks <maksat.omirzak9410@gmail.com>
2021-08-26 17:36:20 +0500

    функция

--- Program.cs
+++ Program.cs
@@ -8,79 +8,103 @@ namespace ConsoleApp3
 {
     class Program
     {
-        static void Main(string[] args)
+        static void esep1()
         {
-
-            Console.Write("a=");
+             Console.Write("a=");
             int a = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"1-esep P={4 * a}");
-            Console.WriteLine("--------------------------------");
-            //--------------------------------
-
+        }
+        static void esep2()
+        {   
+            Console.Write("a=");
+            int a = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"2-esep S={Math.Pow(a, 2)} ");
-            Console.WriteLine("--------------------------------");
-            //--------------------------------
-
+        }
+         static void esep3()
+         {
+            Console.Write("a=");
+            int a = Convert.ToInt32(Console.ReadLine());
             Console.Write("b=");
             int b = int.Parse(Console.ReadLine());
             Console.WriteLine($"3-esep S=a*b={a * b} P=2(a+b)={2 * (a + b)}");
-            Console.WriteLine("--------------------------------");
-            //--------------------------------
-
+          }
+        static void esep4()
+         {
             Console.Write("d=");
             int d = int.Parse(Console.ReadLine());
             Console.WriteLine("4-esep L=" + Math.PI * d);
-            Console.WriteLine("--------------------------------");
-            //--------------------------------
-
+        }
+        static void esep5()
+         {
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
             Console.WriteLine("5-esep V=a^3=" + Math.Pow(a, 3) + "  S=6a^2=" + 6 * Math.Pow(a, 2));
-            Console.WriteLine("--------------------------------");
-            //--------------------------------
-
+            }
+         static void esep6()
+            {
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
             Console.Write("c=");
-            int c = Convert.ToInt32(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
             Console.WriteLine($"6-esep V=abc={ a * b * c}  S=2(ab+bc+ac)= {2 * (a * b + b * c + a * c)}");
-            Console.WriteLine("--------------------------------");
-            //--------------------------------
-
+             }
+        static  void esep7()
+            {
             Console.Write("R=");
             int R = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"7-esep L=2*pi*R={2 * Math.PI * R} S=pi*R^2={Math.PI * R * R} ");
             Console.WriteLine("--------------------------------");
             //--------------------------------
-
-            int a1 = 5;
-            Console.WriteLine("a1=" + a1);
-            int b1 = 7;
-            Console.WriteLine($"b1={b1}");
-            Console.WriteLine($"8-esep (a1+b1)/2={(a1 + b1) / 2}");
+            }
+        static  void esep8()
+            {
+            int a = 5;
+            Console.WriteLine("a=" + a);
+            int b = 7;
+            Console.WriteLine($"b={b}");
+            Console.WriteLine($"8-esep (a+b)/2={(a + b) / 2}");
+            Console.WriteLine("--------------------------------");
+             }
+        static  void esep9()
+            {
+            int a = 5;
+            Console.WriteLine("a=" + a);
+            int b = 7;
+            Console.WriteLine($"b={b}");
+            Console.WriteLine($"9-esep sqrt(a*b)={Math.Sqrt(a * b)}");
+             }
+        static  void esep10()
+            {
+            int a = 5;
+            Console.WriteLine("a=" + a);
+            int b = 7;
+            Console.WriteLine($"b={b}");
+            Console.WriteLine($"10-esep a+b={a + b} a1-b1={a - b} a*b={a * b} a^2={Math.Pow(a, 2)} b^2={Math.Pow(b, 2)}");
             Console.WriteLine("--------------------------------");
             //--------------------------------
-
-            Console.WriteLine($"9-esep sqrt(a1*b1)={Math.Sqrt(a1 * b1)}");
+             }
+        static  void esep11()
+            {
+            int a = 10;
+            Console.WriteLine("a=" + a);
+            int b = -20;
+            Console.WriteLine($"b={b}");
+            Console.WriteLine($"11-esep a+b={a + b} a-b={a - b} a*b={a * b} a^2={Math.Abs(a)} b^2={Math.Abs(b)} ");
+             }
+        static  void esep12()
+            {
+            int a = 8;
+            Console.WriteLine("a=" + a);
+            int b = 6;
+            Console.WriteLine($"b={b}");
+            double c = Math.Sqrt(a * a + b * b);
+            Console.WriteLine($"12-esep c=sqrt(a^2+b^2)={c}  P=a+b+c={a + b + c}");
             Console.WriteLine("--------------------------------");
-            //--------------------------------
-            Console.WriteLine($"10-esep a1+b1={a1 + b1} a1-b1={a1 - b1} a1*b1={a1 * b1} a1^2={Math.Pow(a1, 2)} b1^2={Math.Pow(b1, 2)}");
-            Console.WriteLine("--------------------------------");
-            //--------------------------------
-
-            int a2 = 10;
-            Console.WriteLine("a2=" + a2);
-            int b2 = -20;
-            Console.WriteLine($"b2={b2}");
-            Console.WriteLine($"11-esep a2+b2={a2 + b2} a2-b2={a2 - b2} a2*b2={a2 * b2} a^2={Math.Abs(a2)} b2^2={Math.Abs(b2)} ");
-            Console.WriteLine("--------------------------------");
-            //--------------------------------
-
-            int a3 = 8;
-            Console.WriteLine("a3=" + a3);
-            int b3 = 6;
-            Console.WriteLine($"b3={b3}");
-            double c3 = Math.Sqrt(a * a + b * b);
-            Console.WriteLine($"12-esep c=sqrt(a3^2+b3^2)={c3}  P=a3+b3+c3={a3 + b3 + c3}");
-            Console.WriteLine("--------------------------------");
-            //--------------------------------
-
+             }
+        static  void esep13()
+            {
             int R1 = 10;
             int R2 = 5;
             double S1 = Math.PI * R1 * R1;
@@ -88,38 +112,44 @@ namespace ConsoleApp3
             double S3 = S1 - S2;
             Console.WriteLine($"13-esep S1=pi*R1^2={S1} S2=pi*R2^2={S2} S3=S1-S2={S3} ");
             Console.WriteLine("--------------------------------");
-            //--------------------------------
+             }
+        static  void esep14()
+            {
             int L = 100;
-            double R4 = L / Math.PI * 2;
-            double S4 = Math.PI * R4 * R4;
+            double R = L / Math.PI * 2;
+            double S = Math.PI * R * R;
             Console.WriteLine($"L={L}");
-            Console.WriteLine($"R4={R4}");
-            Console.WriteLine($"14-esep S4={S4}");
+            Console.WriteLine($"R={R}");
+            Console.WriteLine($"14-esep S={S}");
             Console.WriteLine("--------------------------------");
             //--------------------------------
-
-            int S5 = 80;
-            double R5 = S5 / Math.PI;
-            double D5 = R5 * 2;
-            double L5 = 2 * Math.PI * R5;
-            Console.WriteLine($"15-esep D5={D5} L5={L5}");
+             }
+        static  void esep15()
+            {
+            int S = 80;
+            double R = S / Math.PI;
+            double D = R * 2;
+            double L = 2 * Math.PI * R;
+            Console.WriteLine($"15-esep D={D} L={L}");
             Console.WriteLine("--------------------------------");
-            //--------------------------------
-
+             }
+        static  void esep16()
+            {
             int x1 = 5;
             int x2 = -12;
             Console.WriteLine($"16-esep |x2-x1|={Math.Abs(x2 - x1)}");
             Console.WriteLine("--------------------------------");
             //--------------------------------
+             }
+        static  void esep17()
+            {
 
-            int A1 = 3;
-            int B1 = 5;
-            int C1 = 7;
+             }
+        static void Main(string[] args)
+        {
+            
+            esep1();
 
-
-            Console.WriteLine("--------------------------------");
-            //--------------------------------
-            Console.ReadKey();
             Console.ReadKey(); 
         
         }
